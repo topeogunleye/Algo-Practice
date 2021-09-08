a52ded9f6ee19650ff944fd666d82e74a7579c31
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-08 21:14:47 +0100

    Adds solution to seekAndDestroy.js

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -1,17 +1,37 @@
-function destroyer(arr) {
-  let valsToRemove = Object.values(arguments).slice(1)
+function translatePigLatin(str) {
+  
+  if (str[0].match(/[aeiou]/)) {
+    str = str + "way"
+    // console.log(str)
+  } else if (str[0].match(/[bcdfghjklmnpqrstvwxys]/gi)) {
+    str = str.split("")
 
-  for(let i = 0; i < arr.length; i++) {
-     for (let j = 0; i < valsToRemove.length; j++) {
-       if (arr[i] === valsToRemove[j]) {
-          delete arr[i]
-       }
-     }
+    // Create an array of first consonants before a vowel
+    let firstConsonants = []
+    for (let i = 0; i < str.length; i++) {
+      firstConsonants.push(str[i]);
+      if ((str[i]).match(/[aeiou]/)) {
+          firstConsonants.pop()
+          break;
+      }
+    } 
+    console.log(firstConsonants)
+
+    // using the length of first consonants
+    // remove everything after the first consonants from 'str'
+    // str is now contains "first consonants" only
+    let afterFirstConsonants = str.splice(firstConsonants.length)  
+
+    console.log(afterFirstConsonants)
+
+    str = afterFirstConsonants.join("") + firstConsonants.join("") + 'ay'
+    console.log(str)
+  } 
+  
+  return str;
+  
   }
 
-  let newArr = arr.filter(item => item !== null);
-  console.log(newArr)
-
-}
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+  translatePigLatin("rhythm")
+  translatePigLatin("california")
+  translatePigLatin("glove")
