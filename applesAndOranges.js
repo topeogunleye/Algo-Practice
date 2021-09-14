479d28338afc62c0656cd40a1217338dadc82353
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-14 07:12:22 +0100

    adds apples And Oranges algorithm solution

--- applesAndOranges.js
+++ applesAndOranges.js
@@ -20,27 +20,26 @@
 // countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
 
 
+
+
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // Write your code here
   let applesOnHouse = 0;
   let orangesOnHouse = 0;
-  
+
   for (var i = 0; i < apples.length; i++) {
       if (a + apples[i] >= s && a + apples[i] <= t) {
           applesOnHouse++
-          // return applesOnHouse
       }
   }
   
-  for (var j = 0; j < apples.length; j++) {
+  for (var j = 0; j < oranges.length; j++) {
       if (b + oranges[j] <= t && b + oranges[j] >= s) {
           orangesOnHouse++
-          // return orangesOnHouse
       }
   }
   
-  let result = (`${applesOnHouse}\n${orangesOnHouse}`)
-  console.log(result)
+  console.log(`${applesOnHouse}\n${orangesOnHouse}`)
 }
 
 
