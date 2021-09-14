31c7c223c24fda6f575fc8fd5fb4a102dff6a3b2
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-14 07:13:33 +0100

    adds numberLineJumbs.js

---
+++ numberLineJumps.js
@@ -0,0 +1,15 @@
+function kangaroo(x1, v1, x2, v2) {
+  let result = '';
+  let firstRun = '';
+  let secondRun = '';
+  
+  // Write your code here
+  if ((v2 > v1) && (x2 > x1)) {
+      result = "NO"
+  } else if (((v2 < v1) || (x2 < x1))) {
+      // for (let  i = v1; v1 )
+  }
+  
+  
+ return result;
+}
