645bc8615df4d3708e6aa1a4018feca20df5a99c
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-14 09:17:58 +0100

    removes commented out code

--- numberLineJumps.js
+++ numberLineJumps.js
@@ -21,27 +21,3 @@ function kangaroo(x1, v1, x2, v2) {
 }
 
 kangaroo(0, 2, 5, 3);
-
-
-
-// function kangaroo(x1, v1, x2, v2) {
-//   let result = '';
-//   let firstRun = x1;
-//   let secondRun = x2;
-  
-//   // Write your code here
-//   if ((v2 > v1) && (x2 > x1)) {
-//       result = "NO"
-//   } else if ((v2 < v1) || (x2 < x1)) {
-//       for (let  i = 0; secondRun !== firstRun; i++) {
-//           firstRun += v1;
-//           secondRun += v2;
-//       }
-//       if (secondRun === firstRun) {
-//           result = "YES"
-//       }
-//   }
-  
-  
-//  return result;
-// }
