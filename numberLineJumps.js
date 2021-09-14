ac01d603a9eeb5bdaed49d3658d432f9d5f02ce0
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-14 08:36:48 +0100

    adds numberLineJumps solution

--- numberLineJumps.js
+++ numberLineJumps.js
@@ -1,15 +1,47 @@
 function kangaroo(x1, v1, x2, v2) {
   let result = '';
-  let firstRun = '';
-  let secondRun = '';
-  
+  let firstRun = x1;
+  let secondRun = x2;
+
   // Write your code here
-  if ((v2 > v1) && (x2 > x1)) {
-      result = "NO"
-  } else if (((v2 < v1) || (x2 < x1))) {
-      // for (let  i = v1; v1 )
+
+  for (let i = 0; i <= 10000; i++) {
+    firstRun += v1;
+    secondRun += v2;
+
+    if (secondRun === firstRun) {
+      result = 'YES'
+      break;
+    } else {
+      result = 'NO'
+    }
   }
-  
-  
- return result;
+  console.log(firstRun, secondRun);
+  console.log(result);
 }
+
+kangaroo(0, 2, 5, 3);
+
+
+
+// function kangaroo(x1, v1, x2, v2) {
+//   let result = '';
+//   let firstRun = x1;
+//   let secondRun = x2;
+  
+//   // Write your code here
+//   if ((v2 > v1) && (x2 > x1)) {
+//       result = "NO"
+//   } else if ((v2 < v1) || (x2 < x1)) {
+//       for (let  i = 0; secondRun !== firstRun; i++) {
+//           firstRun += v1;
+//           secondRun += v2;
+//       }
+//       if (secondRun === firstRun) {
+//           result = "YES"
+//       }
+//   }
+  
+  
+//  return result;
+// }
