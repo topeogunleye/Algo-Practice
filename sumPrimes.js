0cfa0521f3997143ce1a2849b282590b7df6bff2
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-09-19 20:24:28 +0100

    adds sumPrimes.js

---
+++ sumPrimes.js
@@ -0,0 +1,33 @@
+function sumPrimes(num) {
+  let list = [];
+  let primeList = [];
+  let notPrimeList = [];
+
+  for (var i = 2; i <= num; i++) {
+    list.push(i);
+    checkPrime(i);
+  }
+  // console.log(list)
+
+  function checkPrime(number) {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        notPrimeList.push(number);
+        return false;
+      }
+    }
+    primeList.push(number);
+    return true;
+  }
+
+  console.log(notPrimeList)
+  console.log(primeList);
+
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+  return primeList.reduce(reducer);
+}
+
+sumPrimes(10);
+
+
