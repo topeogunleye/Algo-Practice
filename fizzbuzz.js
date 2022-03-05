fe105de9acd68a3483afd04474d061cf11ea0cf3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-05 17:00:29 +0100

    Add solution to fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+const palidrome = (str) => {
+  // write your code here
+
+  str = str.toLowerCase();
+  let reverseStr = str.split('').reverse().join('');
+  reverseStr = reverseStr.toLowerCase()
+  
+  console.log(reverseStr)
+
+  if (reverseStr === str) return true;
+  return false;
+};
+
+palidrome('Racecar')
