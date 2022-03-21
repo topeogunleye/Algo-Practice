737a0a4d79de38926d151109658347229d1d3593
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-21 04:15:00 +0100

    Define LinkedList class and Node class with constructors

---
+++ doubly-linkedlist.js
@@ -0,0 +1,22 @@
+//  Double Linked List
+
+class LinkedList {
+  constructor() {
+    this.head = this.tail = null
+  }
+
+  // add to end of list / tail
+  append() {
+
+  }
+
+  // a
+}
+
+class Node {
+  constructor(value, prev, next) {
+    this.value = value
+    this.prev = prev || null
+    this.next = next || null
+  }
+}
