bf08df3f8e30258fdb2e9c5338c304f2f2fe25d9
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-21 05:45:46 +0100

    Add LOGIC for searching

--- doubly-linkedlist.js
+++ doubly-linkedlist.js
@@ -2,19 +2,19 @@
 
 class LinkedList {
   constructor() {
-    this.head = this.tail = null
+    this.head = this.tail = null;
   }
 
   // add to end of list / tail
   append(value) {
     // if list is empty
     if (!this.tail) {
-      this.head = this.tail = new Node(value)
+      this.head = this.tail = new Node(value);
     } else {
-      let oldTail = this.tail
-      this.tail = new Node(value)
-      oldTail.next = this.tail
-      this.tail.previous = oldTail
+      let oldTail = this.tail;
+      this.tail = new Node(value);
+      oldTail.next = this.tail;
+      this.tail.previous = oldTail;
     }
   }
 
@@ -22,59 +22,67 @@ class LinkedList {
   prepend(value) {
     // if list is empty
     if (!this.head) {
-      this.head = this.tail = new Node(value)
+      this.head = this.tail = new Node(value);
     } else {
-      let oldHead = this.head
-      this.head = new Node(value)
-      oldHead.prev = this.head
-      this.head.next = oldHead
+      let oldHead = this.head;
+      this.head = new Node(value);
+      oldHead.prev = this.head;
+      this.head.next = oldHead;
     }
-
   }
 
   deleteHead() {
     // if list is empty
-    if(!this.head) {
-      return null
+    if (!this.head) {
+      return null;
     } else {
-      let removeHead = this.head
+      let removeHead = this.head;
       // if 1 node left
       if (this.head === this.tail) {
-        this.head = this.tail = null
+        this.head = this.tail = null;
       } else {
-        this.head = this.head.next
-        this.head.prev = null
+        this.head = this.head.next;
+        this.head.prev = null;
       }
-      return removeHead.value
+      return removeHead.value;
     }
   }
 
   deleteTail() {
     // if list is empty
-    if(!this.tail) {
-      return null
+    if (!this.tail) {
+      return null;
     } else {
-      let removeTail = this.tail
+      let removeTail = this.tail;
       // if 1 node left
       if (this.head === this.tail) {
-        this.head = this.tail = null
+        this.head = this.tail = null;
       } else {
-        this.tail = this.tail.prev
-        this.tail.next = null
+        this.tail = this.tail.prev;
+        this.tail.next = null;
       }
-      return removeTail.value
+      return removeTail.value;
     }
   }
 
-  search() {
+  search(value) {
+    let currentNode = this.head;
 
+    while (currentNode) {
+      if (currentNod.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return null;
   }
 }
 
 class Node {
   constructor(value, prev, next) {
-    this.value = value
-    this.prev = prev || null
-    this.next = next || null
+    this.value = value;
+    this.prev = prev || null;
+    this.next = next || null;
   }
 }
