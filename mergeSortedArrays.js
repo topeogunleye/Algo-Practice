e26a3138c0b9a6a99b4c38fe3401ed5df7639bd3
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-03 21:56:53 +0100

    Add merge sorted array

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,8 +1,37 @@
-const mergeSortedArrays = (arr1, arr2) => {
-  mergedArray = [...arr1, ...arr2];
+const mergeSortedArrays = (array1, array2) => {
 
-  return mergedArray.sort((a, b) => a - b)
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 0;
+  let j = 0;
+ 
+  // check inputs
+  if (array1.length === 0) return array2;
 
+  if (array2.length === 0) return array1;
+
+  function pushFirstArrayItem () {
+    mergedArray.push(array1Item)
+    array1Item = array1[i]
+    i++
+  }
+
+  function pushSecondArrayItem () {
+    mergedArray.push(array2Item)
+    array2Item = array2[j]
+    j++
+  }
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      pushFirstArrayItem()
+    } else {
+      pushSecondArrayItem()
+    }
+  }
+
+  return mergedArray;
 }
 
 console.log(mergeSortedArrays([0,3,4,31], [4,6,30]))
