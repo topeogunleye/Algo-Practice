7adca9ecf20fcc49a86a795f58b60b22ab93e200
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-04 21:45:44 +0100

    Add hashTables

---
+++ algorithms/hashTables.js
@@ -0,0 +1,14 @@
+class HashTable {
+  setItem = (key, value) => {
+
+  }
+
+  getItem = (key) => {
+    return ""
+  }
+
+}
+
+const myTable = new HashTable();
+myTable.setItem('firstName', 'Bob');
+console.log(myTable.getItem('firstName'));
