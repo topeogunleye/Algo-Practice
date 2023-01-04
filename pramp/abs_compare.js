27dff06571d376c2ad8f56d716f520e2efb05e2e
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-04 22:16:36 +0100

    add abs sort

---
+++ pramp/abs_compare.js
@@ -0,0 +1,28 @@
+function absSort(arr) {
+	/**
+	@param arr: integer[]
+	@return: integer[]
+	*/
+  
+// Input:
+ 
+// [2,-7,-2,-2,0]
+// Expected:
+ 
+// [0,-2,-2,2,-7]
+// Actual:
+ 
+// [ 0, 2, -2, -2, -7 ]
+
+  const abs_compare = (a,b) => {
+     if (Math.abs(a) > Math.abs(b)) return true
+    else if (Math.abs(a) < Math.abs(b)) return false
+    else return a>b
+  }
+  
+  arr.sort((a,b) => abs_compare(a,b))
+  return arr
+	// your code goes here
+}
+
+
