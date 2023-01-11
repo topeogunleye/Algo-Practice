a650707adddf806b5fa02d06d846060596fc773e
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-11 22:08:25 +0100

    Add firstRecurring Numbers

--- algorithms/firstRecurringNumbers.js
+++ algorithms/firstRecurringNumbers.js
@@ -2,17 +2,22 @@
 
 const firstRecurringNumbers = (input) => {
 
-    // create an object to stor
-    let map = {}
+    // create an object
+    // check if current input is in input
+    // if current input is in object, return current input,
+    // else set object to current input
+    // return undefined if no input repeats itself
+    let obj = {}
 
-    for (let i = 0; i < input.length; i++) {
-        if (map[input[i]] !== undefined) {
-            return input[i]
-        } else {
-            map[input[i]] = i
-        }
+   for (let i = 0; i < input.length; i++) {
+    if (obj[input[i]] !== undefined) {
+        return input[i]
+    } else {
+        obj[input[i]] = i
     }
-    return undefined
+   }
+
+   return undefined
 
 }
 
@@ -20,3 +25,5 @@ const firstRecurringNumbers = (input) => {
 // [2,5,1,2,3,5,1,2,4] => 2
 
 console.log(firstRecurringNumbers([2,5,1,2,3,5,1,2,4])) // 2
+
+
