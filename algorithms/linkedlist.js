7af029fda6b88492990f1339bd49495233f7f24b
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-01-14 22:00:54 +0100

    Update LinkedList

--- algorithms/linkedlist.js
+++ algorithms/linkedlist.js
@@ -32,10 +32,74 @@ class LinkedList {
     return this
   }
 
+  printList() {
+    const array = []
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array)
+    return array
+  }
+
+  insert(index, value) {
+
+    if (index >= this.length) {
+      return this.append(value)
+    }
+    
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+
+    if (index === this.length - 1) {
+      this.append(value)
+      return this.printList();
+    }
+
+    const newNode = {
+      value: value,
+      next: null
+    }
+
+    const leader = this.traverseToIndex(index-1)
+    const holdingPointer = leader.next
+    leader.next = newNode
+    newNode.next = holdingPointer
+    this.length++
+    return this.printList()
+    // console.log(leader)
+  }
+
+  remove(index) {
+    const leader = this.traverseToIndex(index-1)
+    const pointTO = this.traverseToIndex(index+1)
+    leader.next = pointTO;
+    this.length--
+    return this.printList()
+  }
+
+
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++
+    }
+    return currentNode
+  }
+
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+myLinkedList.printList()
+myLinkedList.insert(2,99)
+myLinkedList.remove(2)
 console.log(myLinkedList)
