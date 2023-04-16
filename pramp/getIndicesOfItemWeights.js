224b2236c8f629d3f10750f5d38cc145529d2473
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-04-16 22:02:22 +0100

    Add Merging 2 Packages solution

---
+++ pramp/getIndicesOfItemWeights.js
@@ -0,0 +1,53 @@
+function getIndicesOfItemWeights(arr, limit) {
+  // let result;
+  // [4, 6, 10, 15, 10]
+  // arr to store the indices
+  let indices = {}
+  
+  // [4, 4, 1], 5
+  
+  // 5 - 4 = 1   is 1 in hashmap? 
+  // build hashmap k:v  { 4: [0,1], 1: [2]}
+  
+  // loop through our array
+  for (let i = 0; i < arr.length; i++) {
+    
+    
+    let diff = limit - arr[i]
+    
+    // if diff is in indices, return [indices[diff], i]
+    if (indices[diff] !== undefined) {
+      return [i, indices[diff]]
+    } else {
+      indices[arr[i]] = i
+    }
+  }
+
+  return []
+}
+
+console.log(getIndicesOfItemWeights([4, 4, 1], 5))
+                                  
+/*
+
+pair [a,b]
+a > b   a,b
+b > a   b,a
+
+
+[4, 6, 10, 15, 16]/ 
+ const arr = {
+4: true
+6: true
+
+}
+
+for outer i =0
+  for inner j=i+1
+    add both == limit
+
+*/
+
+
+
+
