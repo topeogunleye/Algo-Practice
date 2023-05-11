eb3da6e945e022a09c1a9c007b9b2cd7d256cf25
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-11 22:13:05 +0100

    Add function to reverse words in an array
    - Implement a two-step algorithm using a helper function
    - First, reverse the whole array
    - Second, reverse each word in the array
    - Return the modified array

---
+++ pramp/sentenceReverse.js
@@ -0,0 +1,25 @@
+function reverseWords(arr) {
+  let n = arr.length;
+ let start = 0;
+ reverse(arr, 0, n-1);
+ for (i=0;i<n;i++) {
+   if (arr[i] === " ") {
+     reverse(arr,start,i-1);
+     start = i+1;
+   } else if (i === n-1) {
+     reverse(arr,start,n-1);
+   }
+ }
+ return arr;
+}
+
+function reverse(arr, start, end) {
+ let temp;
+ while (end > start) {
+   temp = arr[start];
+   arr[start] = arr[end];
+   arr[end] = temp;
+   start++;
+   end--;
+ };
+}
