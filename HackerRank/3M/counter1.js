0af9b932f44e64550b2f5a38f4bcd1c3fa7c7642
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-06-25 22:47:36 +0100

    Add explanation to countingSort

--- HackerRank/3M/counter1.js
+++ HackerRank/3M/counter1.js
@@ -46,6 +46,21 @@
 // Returns
 // int[100]: a frequency array
 
+// This code is a function that takes an array of integers as input and 
+// returns an array of integers representing the frequency of each value in the input array. 
+// The function first initializes a new array called freq with 100 zeros using the fill method. 
+// This is because the input array is assumed to contain integers between 0 and 99.
+
+// The function then iterates over each element in the input array using a for loop. 
+// For each element, the function increments the counter at the index of the value in the freq array. 
+// This means that if the input array contains the value 5, 
+// the counter at index 5 in the freq array will be incremented by 1.
+
+// Finally, the function returns the freq array, which now contains the frequency of each value in 
+// the input array. For example, if the input array was [1, 2, 2, 3, 3, 3], 
+// the freq array returned by the function would be [0, 1, 2, 3, 0, 0, ..., 0], 
+// where the value at index 1 is 1, the value at index 2 is 2, and the value at index 3 is 3.
+
 function countingSort(arr) {
     // Write your code here
     const freq = new Array(100).fill(0); // initialize frequency array with 100 zeros
